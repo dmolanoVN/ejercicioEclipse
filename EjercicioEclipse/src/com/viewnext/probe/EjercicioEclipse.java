2ecf8728add3c9b9b89c7c3121f204d5e180b90a
Dionisio Molano-Robledo <dmolano@viewnext.com>
2021-05-30 20:51:37 +0200

    Cuarto commit

--- EjercicioEclipse/src/com/viewnext/probe/EjercicioEclipse.java
+++ EjercicioEclipse/src/com/viewnext/probe/EjercicioEclipse.java
@@ -5,6 +5,7 @@ public class EjercicioEclipse {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 		System.out.println("¿Qué tal?");
+		System.out.println("¿Me has escuchado?");
 		System.out.println("muy bien");
 	}
 }
